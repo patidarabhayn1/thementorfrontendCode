397a9841bc6c063227a54ac36776d778af9b5f20
Abhay Patidar <patidarabhayn1@gmail.com>
2021-10-30 22:17:32 +0530

    Minor Changes Needed Now Only

--- mentor-frontend/src/component/Teacher/MainComponent.js
+++ mentor-frontend/src/component/Teacher/MainComponent.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Header from './HeaderComponent';
 import NavigationBar from './NavbarComponent';
 import { Route, Redirect, Switch} from 'react-router-dom';
